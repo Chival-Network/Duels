65c290b198c0bc0baa81c7c460c7cfcb954994a9
BuildTools <unconfigured@null.spigotmc.org>
2023-01-19 12:46:45 +1300

    Add files

--- src/main/java/au/chival/duels/Duels.java
+++ src/main/java/au/chival/duels/Duels.java
@@ -2,17 +2,16 @@ package au.chival.duels;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Main extends JavaPlugin {
+public final class Duels extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
+        // Plugin startup logic
 
     }
 
     @Override
     public void onDisable() {
-
-
+        // Plugin shutdown logic
     }
 }
